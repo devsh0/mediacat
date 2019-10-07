88380749d25d7ba64f7008ff324cb576a0456bcf
devashish <devashishjaiswal86@gmail.com>
2019-10-07 10:38:58 +0530

    Removed picocli dependency from module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module org.mediacat {
     requires transitive org.jsoup;
     requires transitive jdk.crypto.ec;
-    requires info.picocli;
     exports org.mediacat.torrent;
     exports org.mediacat.settings;
     exports org.mediacat.utils;
