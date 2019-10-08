c16d78f10652286f517c04db116ef3f6f395f80d
devashish <devashishjaiswal86@gmail.com>
2019-10-08 13:22:32 +0530

    Added a static ProxySelector that selects proxies based on baseUrl.
    Made necessary changes to make AbstractEngine thread safe. Added a
    method that deals with HTTP requests and returns Jsoup Document
    object. This method is compatible with proxies.

--- src/main/java/org/mediacat/torrent/AbstractEngine.java
+++ src/main/java/org/mediacat/torrent/AbstractEngine.java
@@ -1,53 +1,104 @@
 package org.mediacat.torrent;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
 import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.net.http.HttpResponse.BodyHandlers;
 
 abstract public class AbstractEngine implements TorrentEngine {
-    protected volatile String baseUrl;
-    protected volatile String searchPath;
-    protected volatile Proxy proxy;
-    protected volatile boolean isFailing;
+    private static final Object LOCK = new Object();
+    private static final Map<String, Proxy> proxyMap = new HashMap<>();
+    private static final ProxySelector proxySelector = new TorrentProxySelector(proxyMap);
+
+    protected String baseUrl;
+    protected String searchPath;
+    protected Proxy proxy;
+    protected boolean isFailing;
 
     public AbstractEngine (String baseUrl, String searchPath, Proxy proxy) {
-        this.baseUrl = baseUrl;
-        this.searchPath = searchPath;
-        this.proxy = proxy;
+        synchronized (LOCK) {
+            this.baseUrl = baseUrl;
+            this.searchPath = searchPath;
+            this.proxy = proxy;
+            proxyMap.put(baseUrl, proxy);
+        }
     }
 
     // Getters
     @Override
     public String getBaseUrl() {
-        return baseUrl;
+        synchronized (LOCK) {
+            return baseUrl;
+        }
     }
 
     @Override
     public String getSearchPath() {
-        return searchPath;
+        synchronized (LOCK) {
+            return searchPath;
+        }
     }
 
     @Override
     public Proxy getProxy() {
-        return proxy;
+        synchronized (LOCK) {
+            return proxy;
+        }
     }
 
     // Setters
     @Override
     public void setBaseUrl(String baseUrl) {
-        this.baseUrl = baseUrl;
+        synchronized (LOCK) {
+            Proxy proxy = proxyMap.remove(this.baseUrl);
+            this.baseUrl = baseUrl;
+            proxyMap.put(baseUrl, proxy);
+        }
     }
 
     @Override
     public void setSearchPath(String searchPath) {
-        this.searchPath = searchPath;
+        synchronized (LOCK) {
+            this.searchPath = searchPath;
+        }
     }
 
     @Override
     public void setProxy(Proxy proxy) {
-        this.proxy = proxy;
+        synchronized (LOCK) {
+            this.proxy = proxy;
+            proxyMap.put(baseUrl, proxy);
+        }
     }
 
     @Override
     public boolean isFailing() {
-        return isFailing;
+        synchronized (LOCK) {
+            return isFailing;
+        }
+    }
+
+    protected Document getDocument (String url) throws IOException, InterruptedException {
+        synchronized (LOCK) {
+            HttpClient client = HttpClient.newBuilder()
+                    .proxy(proxySelector)
+                    .build();
+
+            HttpRequest req = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .build();
+
+            String body = client.send(req, BodyHandlers.ofString()).body();
+            return Jsoup.parse(body);
+        }
     }
 }
