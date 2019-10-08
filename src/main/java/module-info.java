31f42411170ab53b16bb172b1e6aaf8b57e1bb54
devashish <devashishjaiswal86@gmail.com>
2019-10-08 13:17:25 +0530

    Added module dependency to java.net.http

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.mediacat {
     requires transitive org.jsoup;
     requires transitive jdk.crypto.ec;
+    requires transitive java.net.http;
     exports org.mediacat.torrent;
     exports org.mediacat.settings;
     exports org.mediacat.utils;
