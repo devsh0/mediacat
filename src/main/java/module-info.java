14bcbde938932c1df0eeaab17488ccfa4d1cdaee
devashish <devashishjaiswal86@gmail.com>
2019-11-22 18:53:56 +0530

    Added picocli in the module-info file.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,11 @@ module org.mediacat {
     requires transitive org.jsoup;
     requires transitive jdk.crypto.ec;
     requires transitive java.net.http;
+    requires info.picocli;
     exports org.mediacat.torrent;
     exports org.mediacat.settings;
     exports org.mediacat.utils;
     exports org.mediacat.filter;
+    exports org.mediacat.cli;
+    opens org.mediacat.cli;
 }
